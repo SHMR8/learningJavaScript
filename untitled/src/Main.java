7d2db55e394b8ecac96fd43405060bdf8e6d0c19
Samuel Moreira <samuelhmrocha@gmail.com>
2023-02-05 16:36:34 -0300

    Exercice OO

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -2,23 +2,16 @@
 public class Main {
     public static void main(String[] args) {
 
-    double [][] notasAlunos = new double[2][4];
+    Carro car = new Carro();
+    car.marca = "Porsche";
+    car.modelo = "Cayenne";
+    car.numDePassageiros = 2;
 
-        notasAlunos[0][0]=1;
-        notasAlunos[0][1]=2;
-        notasAlunos[0][2]=3;
-        notasAlunos[0][3]=4;
+    System.out.println(car.marca);
+    System.out.println(car.modelo);
+    System.out.println(car.numDePassageiros);
 
-        notasAlunos[1][0]=5;
-        notasAlunos[1][1]=6;
-        notasAlunos[1][2]=7;
-        notasAlunos[1][3]=8;
 
-        for(int i=0; i<2; i++){
-            for(int j=0; j<4;j++){
-                System.out.println(notasAlunos[i][j]);
-            }
-        }
 
 
     }
