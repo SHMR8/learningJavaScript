5e7cdb04f961ce93804a4a01cb86eee25a98261b
Samuel Moreira <samuelhmrocha@gmail.com>
2023-02-05 16:55:21 -0300

    Exercice 2 - OO

--- untitled/src/Carro.java
+++ untitled/src/Carro.java
@@ -3,4 +3,10 @@ public class Carro {
     public String marca;
     public String modelo;
     public int numDePassageiros;
+    public double capCombustivel;
+    public double consumoCombustivel;
+
+    void exibirDados(){
+      System.out.println("o carro é um: "+marca+" modelo: "+modelo+".");
+    }
 }
--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -3,14 +3,11 @@ public class Main {
     public static void main(String[] args) {
 
     Carro car = new Carro();
+
     car.marca = "Porsche";
-    car.modelo = "Cayenne";
-    car.numDePassageiros = 2;
+    car.modelo="Cayenne";
 
-    System.out.println(car.marca);
-    System.out.println(car.modelo);
-    System.out.println(car.numDePassageiros);
-
+    car.exibirDados();
 
 
 
